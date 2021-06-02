abe753bae361344cb0b2b3305a77fa0c3226c477
khajo <karima.hajo@cergentis.com>
2021-06-02 10:48:48 +0200

    changes in utilities.py, functions get_chr_info, get_re_info, get_read

--- utilities.py
+++ utilities.py
@@ -1,9 +1,8 @@
-
-import json
-
 import numpy as np
 import pandas as pd
-
+from pathlib import Path
+import pysam, re
+import h5py
 
 def overlap(que_item, ref_lst, include_ref_left=False, include_ref_right=False, offset=0):
     if isinstance(que_item, list):
@@ -22,6 +21,7 @@ def overlap(que_item, ref_lst, include_ref_left=False, include_ref_right=False, 
     if que_dim >= 3:  # Chromosome
         has_ovl = np.logical_and(has_ovl, que_item[0] == ref_lst[:, 0])
         crd_ind = 1
+
     if include_ref_left:
         lft_ovl = ref_lst[:, crd_ind] <= (que_item[crd_ind + 1] + offset)
     else:
@@ -34,64 +34,27 @@ def overlap(que_item, ref_lst, include_ref_left=False, include_ref_right=False, 
     return has_ovl
 
 
-def get_chr_info(genome, property='chr_name'):
-    # chr size: https://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes
-    chr_details = dict({
-        'hg19': dict({
-            'chr_name': [
-                'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
-                'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22',
-                'chrX', 'chrY', 'chrM'
-            ],
-            'chr_size': [
-                249250621, 243199373, 198022430, 191154276, 180915260, 171115067, 159138663, 146364022, 141213431, 135534747,
-                135006516, 133851895, 115169878, 107349540, 102531392, 90354753, 81195210, 78077248, 59128983, 63025520, 48129895,
-                51304566, 155270560, 59373566, 16571
-            ]
-        }),
-        'hg38': dict({
-            'chr_name': [
-                'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9',
-                'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18',
-                'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY', 'chrM'
-            ],
-            'chr_size': [
-                248956422, 242193529, 198295559, 190214555, 181538259, 170805979, 159345973, 145138636, 138394717,
-                133797422, 135086622, 133275309, 114364328, 107043718, 101991189, 90338345, 83257441, 80373285,
-                58617616, 64444167, 46709983, 50818468, 156040895, 57227415, 16569
-            ]
-        }),
-        'mm9': dict({
-            'chr_name': [
-                'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
-                'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
-                'chrX', 'chrY', 'chrM'
-            ],
-            'chr_size': [
-                197195432, 181748087, 159599783, 155630120, 152537259, 149517037, 152524553, 131738871, 124076172,
-                129993255, 121843856, 121257530, 120284312, 125194864, 103494974, 98319150, 95272651, 90772031, 61342430,
-                166650296, 15902555, 16299
-            ]
-        }),
-        'mm10': dict({
-            'chr_name': [
-                'chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10',
-                'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19',
-                'chrX', 'chrY', 'chrM'
-            ],
-            'chr_size': [
-                195471971, 182113224, 160039680, 156508116, 151834684, 149736546, 145441459, 129401213, 124595110,
-                130694993, 122082543, 120129022, 120421639, 124902244, 104043685, 98207768, 94987271, 90702639, 61431566,
-                171031299, 91744698, 16299,
-            ]
-        })
-    })
-    return chr_details[genome][property]
+def get_chr_info(reference_fasta, property='chr_name'):
+
+    fasta_path = Path(reference_fasta)
+    fasta_file = pysam.FastaFile(fasta_path) # it will create .fai if not exist as opposed to what the documentation says, raises valueError...
+
+    if fasta_file.nreferences is None: # if empty .fai file...
+        fasta_file.close()
+        fasta_file.unlink()
+        fasta_file = pysam.FastaFile(fasta_path) # to create new index
+
+    if property == "chr_name":
+        return_list = fasta_file.references
+    elif property == "chr_size":
+        return_list = fasta_file.lengths
+
+    fasta_file.close()
+
+    return return_list
 
 
-def get_re_info(re_name='DpnII', property='seq', genome=None, re_path='./renzs/'):
-    from os import path
-
+def get_re_info(re_name='DpnII', property='seq', reference_fasta=None):
     re_details = dict({
         'DpnII': dict({'seq': 'GATC'}),
         'MboI': dict({'seq': 'GATC'}),
@@ -105,16 +68,17 @@ def get_re_info(re_name='DpnII', property='seq', genome=None, re_path='./renzs/'
     })
 
     if property == 'pos':
-        re_fname = path.join(re_path, '{:s}_{:s}.npz'.format(genome, re_name))
-        if not path.isfile(re_fname):
-            extract_re_positions(genome=genome, re_name_lst=[re_name], output_fname=re_fname)
+        re_fname = Path(reference_fasta).with_suffix(".renzs.npz")
+        if re_fname.is_file():
+            print('[w] Restriction enzyme file exists: ' + str(re_fname))
+
+        if not re_fname.is_file():
+            extract_re_positions(re_name_lst=[re_name], output_fname_renzs=re_fname, reference_fasta=reference_fasta)
         re_data = np.load(re_fname, allow_pickle=True)
-        assert np.array_equal(re_data['chr_lst'], get_chr_info(genome=genome, property='chr_name'))
-        assert re_data['genome'] == genome
+        assert np.array_equal(re_data['chr_lst'], get_chr_info(reference_fasta, property='chr_name'))
         return re_data['pos_lst']
     else:
         return re_details[re_name][property]
-
 
 def flatten(nested_lst):
     out_lst = []
@@ -126,38 +90,28 @@ def flatten(nested_lst):
     return out_lst
 
 
-def extract_re_positions(genome, re_name_lst, output_fname=None, ref_fasta=None):
-    from os import path, makedirs
-    import pysam
-    import re
-
+def extract_re_positions(re_name_lst, reference_fasta, output_fname_renzs):
     # Initialization
-    chr_lst = get_chr_info(genome=genome, property='chr_name')
+    chr_lst = get_chr_info(reference_fasta, property='chr_name')
     chr2idx = dict(zip(chr_lst, np.arange(len(chr_lst))))
 
-    if output_fname is None:
-        output_fname = './renzs/{:s}_{:s}.npz'.format(genome, '-'.join(re_name_lst))
-    if path.isfile(output_fname):
+    output_fname_renzs = Path(reference_fasta).with_suffix(".renzs.npz")
+    if output_fname_renzs.is_file():
         print('[w] Restriction enzyme file exists: ' + output_fname)
-        return
-    if not path.isdir(path.dirname(output_fname)):
-        makedirs(path.dirname(output_fname))
-    if ref_fasta is None:
-        with open('./configs.json', 'r') as cnf_fid:
-            ref_fasta = json.load(cnf_fid)['reference_path'][genome]
-    print('Searching in the reference genome defined in: ' + ref_fasta)
+
+    print('Searching in the reference genome defined in: ' + reference_fasta)
 
     # get re sequences
     seq_lst = []
     for re_name in re_name_lst:
-        seq_lst.append(get_re_info(genome=genome, re_name=re_name, property='seq'))
+        seq_lst.append(get_re_info(re_name=re_name, property='seq'))
     re_regex = '|'.join(seq_lst)
 
     # Loop over chromosomes
     re_pos_lst = [None] * len(chr_lst)
     re_type_lst = [None] * len(chr_lst)
     chr_observed = [None] * len(chr_lst)
-    with pysam.FastxFile(path.expanduser(ref_fasta)) as ref_fid:
+    with pysam.FastxFile(reference_fasta) as ref_fid:
         print('Scanning chromosomes for restriction recognition sequences: {:s}'.format(', '.join(seq_lst)))
         for chr_ind, chr in enumerate(ref_fid):
             if not chr.name in chr_lst:
@@ -177,7 +131,7 @@ def extract_re_positions(genome, re_name_lst, output_fname=None, ref_fasta=None)
         print()
 
     # save the result
-    np.savez(output_fname, pos_lst=re_pos_lst, type_lst=re_type_lst, chr_lst=chr_observed, genome=genome, scan_regex=re_regex)
+    np.savez(output_fname_renzs, pos_lst=re_pos_lst, type_lst=re_type_lst, chr_lst=chr_observed, genome=reference_fasta, scan_regex=re_regex)
 
 
 def get_vp_info(run_id, vp_info_path='./vp_info.tsv'):
@@ -194,9 +148,7 @@ def get_vp_info(run_id, vp_info_path='./vp_info.tsv'):
 
 
 def load_dataset(vp_info_lst, target_field='frg_np', data_path='./datasets', verbose=True, vp_width=None,
-                 load_cols=('chr', 'pos', '#read')):
-    import h5py
-    import os
+                 load_cols=('chr_idx', 'pos', '#read')):
 
     if not isinstance(vp_info_lst, list):
         vp_info_lst = [vp_info_lst]
@@ -205,16 +157,16 @@ def load_dataset(vp_info_lst, target_field='frg_np', data_path='./datasets', ver
     if verbose:
         print('Loading data from:')
     for vp_idx, vp_info in enumerate(vp_info_lst):
-        h5_fpath = os.path.join(data_path, vp_info['expr_id'] + '.hdf5')
+        h5_fpath = Path(data_path).joinpath(vp_info['expr_id']).with_suffix('.hdf5')
         if verbose:
             print('\t#{:d}: [{:d}: {:s}]: {:s}'.format(vp_idx + 1, vp_info['row_index'], vp_info['expr_id'], h5_fpath))
-            if (vp_info_lst[0]['vp_chr'] != vp_info['vp_chr']) or (np.abs(vp_info_lst[0]['vp_pos'] - vp_info['vp_pos']) > 1e6):
+            if (vp_info_lst[0]['vp_chr_idx'] != vp_info['vp_chr_idx']) or (np.abs(vp_info_lst[0]['vp_pos'] - vp_info['vp_pos']) > 1e6):
                 print('[w] Viewpoint is far away compared to others runs being loaded.')
 
         # load from hdf5
         with h5py.File(h5_fpath, 'r') as h5_fid:
             # python2: header_lst = list(h5_fid[target_field + '_header_lst'][()])
-            header_lst = [hdr.decode('UTF-8') for hdr in h5_fid[target_field + '_header_lst']]
+            header_lst = [hdr for hdr in h5_fid[target_field + '_header_lst']]
             frg_prt = pd.DataFrame(h5_fid[target_field][()], columns=header_lst)
 
         # only preserve requested columns
@@ -224,7 +176,7 @@ def load_dataset(vp_info_lst, target_field='frg_np', data_path='./datasets', ver
         # remove vp frag-ends?
         if vp_width is not None:
             print('\tRemoving frag-ends closer than {:0,.0f}bp to viewpoint.'.format(vp_width / 2.0))
-            is_nei = (frg_prt['chr'] == vp_info['vp_chr']) & \
+            is_nei = (frg_prt['chr_idx'] == vp_info['vp_chr_idx']) & \
                      (np.abs(frg_prt['pos'] - vp_info['vp_pos']) < vp_width / 2.0)
             frg_prt = frg_prt.loc[~is_nei].reset_index(drop=True)
             print('\t{:,d} frag-ends are removed.'.format(np.sum(is_nei)))
@@ -232,8 +184,8 @@ def load_dataset(vp_info_lst, target_field='frg_np', data_path='./datasets', ver
 
         # report stats
         if verbose:
-            is_cis = frg_prt['chr'] == vp_info['vp_chr']
-            is_far = is_cis & ~ overlap([vp_info['vp_chr'], vp_info['vp_be'], vp_info['vp_en']], frg_prt[['chr', 'pos', 'pos']].values, offset=50e3)
+            is_cis = frg_prt['chr_idx'] == vp_info['vp_chr_idx']
+            is_far = is_cis & ~ overlap([vp_info['vp_chr_idx'], vp_info['vp_be'], vp_info['vp_en']], frg_prt[['chr_idx', 'pos', 'pos']].values, offset=50e3)
             print('\tData stats are:')
             print('\t\t   #fragment (far): {:0,.0f}'.format(np.sum(frg_prt.loc[ is_far, '#read'])))
             print('\t\t   #fragment (cis): {:0,.0f}'.format(np.sum(frg_prt.loc[ is_cis, '#read'])))
@@ -243,7 +195,7 @@ def load_dataset(vp_info_lst, target_field='frg_np', data_path='./datasets', ver
             frg_cmb = frg_prt.copy()
             del frg_prt
         else:
-            agr_hlst = ['chr', 'pos']
+            agr_hlst = ['chr_idx', 'pos']
             if np.array_equal(frg_cmb[agr_hlst], frg_prt[agr_hlst]):
                 print('\t[i] Identical restriction sites detected. Direct addition of coverages...')
                 frg_cmb['#read'] += frg_prt['#read']
@@ -265,6 +217,17 @@ def load_dataset(vp_info_lst, target_field='frg_np', data_path='./datasets', ver
     if verbose and (len(vp_info_lst) != 1):
         print('Final memory usage: {:0,.2f}GB'.format(frg_cmb.memory_usage().sum() / 1e9))
     return frg_cmb.copy()
+
+def str2crd(str_crd, genome='hg19'):
+    chr_lst = get_chr_info(genome=genome, property='chr_name')
+    n_chr = len(chr_lst)
+    chr2nid = dict(zip(chr_lst, range(1, n_chr + 1)))
+
+    items = str_crd.split(':')
+    if len(items) == 1:
+        return [chr2nid[items[0]]]
+    else:
+        return [chr2nid[items[0]]] + [float(x) for x in items[1].split('-')]
 
 
 def perform_sigtest(observed, smoothing_kernel, background=None, n_epoch=1000, nz_only=False, replacement=False):
@@ -346,18 +309,23 @@ class OnlineStats(object):
         return np.sqrt(self.variance)
 
 
-def get_read(input_fid):
-    read = []
-    for frg in input_fid:
-        if len(read) == 0:
-            read = [frg]
+def get_read(sam):
+    return_l = []
+    for read in sam:
+        read_name = read.query_name
+        if read.is_unmapped:
+            continue
+
+        if return_l == list():
+            return_l.append(read)
         else:
-            if read[0].query_name == frg.query_name:
-                read.append(frg)
+            if read_name == return_l[-1].query_name:
+                return_l.append(read)
             else:
-                yield read
-                read = [frg]
-    yield read
+                yield return_l
+                return_l = [read]
+
+    yield return_l
 
 
 
